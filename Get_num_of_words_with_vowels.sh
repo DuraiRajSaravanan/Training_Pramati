715f4dffc20aca1798372278db72e07a8bb584d8
DuraiRajSaravanan <42999715+DuraiRajSaravanan@users.noreply.github.com>
2018-09-11 15:18:32 +0530

    Create Get_num_of_words_with_vowels.sh
    Shell script for getting number of words with vowels in a given document

---
+++ Get_num_of_words_with_vowels.sh
@@ -0,0 +1,12 @@
+ declare -i count=0
+ for j in $(cat test.txt);   
+ do     
+   for (( i=0; i<${#j}; i++ ));      
+     do        
+       if [[ "${j:$i:1}" == [AEIOUaeiou] ]]; then
+         let "count++"
+         break
+    fi   
+     done;   
+ done 
+echo $count
