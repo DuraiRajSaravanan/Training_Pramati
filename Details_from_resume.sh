89b4d0d0e2f62ebb3daa388c982d25f446a40670
DuraiRajSaravanan <42999715+DuraiRajSaravanan@users.noreply.github.com>
2018-09-17 18:16:58 +0530

    Update Details_from_resume.sh

--- Details_from_resume.sh
+++ Details_from_resume.sh
@@ -1,9 +1,16 @@
-grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" testingthemail
-
-grep '\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' testingthemail
-
-grep -o '201[0-8]\{1\}' testingthemail
-
-FILE = "testingthemail"
-
-echo "${FILE%%.*}"
+fpath=$1 
+filename=$(basename "$fpath") 
+directory=$(dirname "$fpath") 
+cd ..
+cd $directory 
+mail=$(grep -E -o "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}" $filename)
+mob=$(grep -E -o "[0-9]{10,12}" $filename)
+echo "Name:$(echo "$filename" | cut -f 1 -d '.')"
+echo "Email:$mail"
+echo "Mobile:$mob"
+res=$filename,$mail,$mob
+echo $res
+sudo docker exec -it container1 bash
+psql -U postgress
+psql -c SELECT * FROM resume_collect;
+psql -c commit;
